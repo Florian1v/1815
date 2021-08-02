44d8aa6709ef75a217258c3f843b1c40dd91ba8f
Nico <qtsddxy@163.com>
2021-08-02 18:15:59 +0800

    fififi

---
+++ precache-manifest.4eb7d09f6d89f50f9948e78590beabe1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86986c9d9798d7704188",
+    "url": "/bookkeeping-vue-website/css/app.a8200ca7.css"
+  },
+  {
+    "revision": "02585af755da632b720cf82bd8ca7f8a",
+    "url": "/bookkeeping-vue-website/index.html"
+  },
+  {
+    "revision": "86986c9d9798d7704188",
+    "url": "/bookkeeping-vue-website/js/app.8896464f.js"
+  },
+  {
+    "revision": "f6e5e001d5ca7fc9aedd",
+    "url": "/bookkeeping-vue-website/js/chunk-vendors.e18fdaba.js"
+  },
+  {
+    "revision": "02adcb277ba6fd882d3307d3966c1370",
+    "url": "/bookkeeping-vue-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bookkeeping-vue-website/robots.txt"
+  }
+]);
